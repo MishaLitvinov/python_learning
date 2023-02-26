6751194ec3e1d079b6777c010ffad93636a7cbd8
AdimSpaceHome <adim.space.money@gmail.com>
2023-02-26 18:38:13 +0200

    bear class

--- bear_in_forest_2.py
+++ bear_in_forest_2.py
@@ -6,45 +6,48 @@ canv=Canvas(window,width=1000,
             height=1000, bg='light blue')
 canv.place(x=0,y=0)
 
-head = canv.create_oval(300, 300, 700, 700,
-                 width=0, fill='Chocolate',outline='SaddleBrown')
-first_ear = canv.create_oval(550,300, 750,500,
-                 width=0, fill='Chocolate',outline='SaddleBrown')
-second_ear = canv.create_oval(250, 300, 450, 500,
-                 width=0, fill='Chocolate',outline='SaddleBrown')
-body = canv.create_oval(250, 600, 750, 1200,
-                 width=0, fill='Chocolate',outline='Black')
-first_eye=canv.create_oval(400, 400, 420, 430,
-                 width=1.5, fill='Blue',outline='Black')
-second_eye=canv.create_oval(580, 400, 600, 430,
-                 width=1.5, fill='Blue',outline='Black')
-mouth=canv.create_arc(400, 475, 600, 600,
-                 extent=-180, style=ARC,width=3,outline='red')
-nose=canv.create_polygon(475, 475, 525, 475, 500, 525,
-        outline='Black', fill="SaddleBrown", width=3)
+class bear:
+    color = 'Chocolate'
 
-def move_bear_parts(x, y):
-    canv.move(head, x, y)
-    canv.move(first_ear, x, y)
-    canv.move(second_ear, x, y)
-    canv.move(first_eye, x, y)
-    canv.move(second_eye, x, y)
-    canv.move(mouth, x, y)
-    canv.move(nose, x, y)
-    canv.move(body, x, y)
+    draw = [];
+    def draw_bear(self):
+        self.draw.append(canv.create_oval(300, 300, 700, 700,
+                                                            width=0, fill='Chocolate',outline='SaddleBrown'))
+        self.draw.append(canv.create_oval(550,300, 750,500,
+                        width=0, fill='Chocolate',outline='SaddleBrown'))
+        self.draw.append(canv.create_oval(250, 300, 450, 500,
+                        width=0, fill='Chocolate',outline='SaddleBrown'))
+        self.draw.append(canv.create_oval(250, 600, 750, 1200,
+                        width=0, fill='Chocolate',outline='Black'))
+        self.draw.append(canv.create_oval(400, 400, 420, 430,
+                        width=1.5, fill='Blue',outline='Black'))
+        self.draw.append(canv.create_oval(580, 400, 600, 430,
+                        width=1.5, fill='Blue',outline='Black'))
+        self.draw.append(canv.create_arc(400, 475, 600, 600,
+                        extent=-180, style=ARC,width=3,outline='red'))
+        self.draw.append(canv.create_polygon(475, 475, 525, 475, 500, 525,
+                        outline='Black', fill="SaddleBrown", width=3))
 
-step=30
 
+    def move(self, x, y):
+        for part in self.draw:
+            canv.move(part, x, y)
+
+
+potapik = bear()
+potapik.draw_bear()
+
+step = 30
 def move_bear(event):
     match event.keysym:
         case 'Up':
-            move_bear_parts(0, -step )
+            potapik.move(0, -step)
         case 'Down':
-            move_bear_parts(0, step )
+            potapik.move(0, step)
         case 'Left':
-            move_bear_parts(-step, 0 )
+            potapik.move(-step, 0)
         case 'Right':
-            move_bear_parts(step, 0 )
+            potapik.move(step, 0)
 
             
     
